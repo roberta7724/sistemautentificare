932c9320aa68cc2533a88c0b048e9fe9bbbb063f
roberta7724 <roberta.bara@emanuel.ro>
2023-10-26 23:16:55 +0300

    initial project setup

---
+++ sistemeautentificare/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace sistemeautentificare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string usernamebun = "beta";
+            const string parolabuna = "787";
+
+            bool corect = false;
+
+            while (!corect)
+            {
+                Console.WriteLine("Introduceti username: ");
+                string username = Console.ReadLine();
+
+                Console.WriteLine("Introduceti parola: ");
+                string parola = Console.ReadLine();
+
+                if (username == usernamebun && parola == parolabuna)
+                {
+                    Console.WriteLine("Bine ai venit, " + usernamebun);
+                    corect = true;
+                }
+                else
+                {
+                    Console.WriteLine("Incorect, incercati din nou");
+                }
+            }
+        }
+    }
+}
